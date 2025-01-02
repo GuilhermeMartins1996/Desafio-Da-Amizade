b11539915344def60f1a6437d7676a2385d8f41e
Guilherme Martins <guilherme.martins.1996@outlook.com>
2025-01-02 11:09:09 -0300

    Troca-de-Sobrenome

---
+++ logica-js-projeto_inicial/Niveldeamizade.js
@@ -0,0 +1,31 @@
+   function funcao()
+ {
+   alert("Descobrindo o nivel de amizade");
+   let nome = prompt("Digite seu nome");
+  
+   if(nome=='Vinicius Fonseca')
+   {
+      alert("Você é um amigo");
+      console.log("Esse é o cara");
+   }
+   else if (nome=='Lucas Tortelli')
+   {
+      alert("Você é um amigo");
+      console.log("Esse é magnata");
+   }
+   else if (nome=='Lucas Pinheiro')
+   {
+      alert("Você é um amigo");
+      console.log("Esse é o mestre");
+   }
+   else if (nome=='Raphael Fonseca')
+   {
+      alert("Você é quase um amigo");
+      console.log("Esse á um safado");
+   }
+   else
+   {
+      alert("Poxa, atualiza a pagina e tente de novo");
+      console.log("Usuario não existe");
+   }
+ }
